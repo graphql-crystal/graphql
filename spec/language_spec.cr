fa9751f4a1d2e863fbb58b4dd78c3c928fd4eada
Jakob Gillich <jakob@gillich.me>
2022-04-11 02:53:30 +0000

    add test for block quote quotes

--- spec/language_spec.cr
+++ spec/language_spec.cr
@@ -1,15 +1,23 @@
 require "./spec_helper"
 
 describe GraphQL::Language do
-  it "parses and serializes query schema" do
+  it "parses and generates query schema" do
     schema = {{ read_file("spec/fixtures/query.graphql") }}.strip
     schema.should eq GraphQL::Language.parse(schema).to_s
-    1.should eq 2
   end
 
-  it "parses and serializes mutation schema" do
+  it "parses and generates mutation schema" do
     schema = {{ read_file("spec/fixtures/mutation.graphql") }}.strip
     schema.should eq GraphQL::Language.parse(schema).to_s
-    1.should eq 2
+  end
+
+  it "parses block string with quote" do
+    schema = %(
+      """
+      Description with quote "
+      """
+      scalar Foo
+    )
+    GraphQL::Language.parse(schema)
   end
 end
