5ad88d7b801e56bd5e629bbedafbb343c557cda0
Chris Watson <cawatson1993@gmail.com>
2023-07-20 11:35:24 -0600

    fix introspection; formatting

--- spec/array_spec.cr
+++ spec/array_spec.cr
@@ -3,27 +3,27 @@ require "./spec_helper"
 module ArrayFixture
   @[GraphQL::Object]
   class Query < GraphQL::BaseQuery
-   @[GraphQL::Field]
-   def nested(arr : Array(Array(Array(String)))) : Array(Array(Array(String)))
-     arr
-   end
+    @[GraphQL::Field]
+    def nested(arr : Array(Array(Array(String)))) : Array(Array(Array(String)))
+      arr
+    end
   end
 end
 
 describe GraphQL do
   it "resolves nested arrays" do
-   GraphQL::Schema.new(ArrayFixture::Query.new).execute(
-     %(
+    GraphQL::Schema.new(ArrayFixture::Query.new).execute(
+      %(
        {
          nested(arr: [[["foo"]]])
        }
      )
-   ).should eq (
-     {
-       "data" => {
-         "nested" => [[["foo"]]],
+    ).should eq (
+      {
+        "data" => {
+          "nested" => [[["foo"]]],
+        },
       }
-     }
-   ).to_json
+    ).to_json
   end
 end
